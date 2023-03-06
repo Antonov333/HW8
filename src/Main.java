24fbef2d9a4aca7446b5d77956ac6da7c7b4a59d
Antonov333 <antonovsergei333@yahoo.com>
2023-03-06 13:07:21 +0300

    Commit to Homework8 with the very beginning of the homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
+    static int hw = 777;
+
     public static void main(String[] args) {
-        System.out.println("\nHello! \n\nJust started with Homework 8");
+        hwTitle();
+        task1();
+
+        hwCompleted();
     }
+
+    public static void hwTitle() {
+        System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №" + hw);
+        System.out.println(" ");
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        System.out.println("Solution to be provided soon");
+        System.out.println("");
+    }
+
+
+    public static void hwCompleted() {
+        System.out.println("Домашнее задание " + hw + " выполнено");
+        System.out.println(" ");
+    }
+
 }
